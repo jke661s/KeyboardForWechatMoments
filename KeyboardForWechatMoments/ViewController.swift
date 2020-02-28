039000c1e2552dcf247d04bcdd2f435ea1aaaebb
Jackie <jke661s@gmail.com>
2020-02-29 09:21:09 +1100

    Project init

---
+++ KeyboardForWechatMoments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyboardForWechatMoments
+//
+//  Created by Jackie Wang on 29/2/20.
+//  Copyright © 2020 Jackie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
